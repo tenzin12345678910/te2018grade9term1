b88385324f753a09aa8638f0b78b8580dacbc4bb
Tenzin12345678910 <tenzinw@csgde.org>
2018-10-20 10:32:01 -0700

    mark dot file

--- change.js
+++ change.js
@@ -1,7 +1,48 @@
-// import readline-sync module, don't touch!
-const READLINE = require("readline-sync");
+const READLINE = require('readline-sync');
 
-// fill in object below with all relevant properties (see instructions)
-let changeMachine = {};
+// declare and assign a variable for pennies using readlineSync
+// ask the user how many pennies he/she wants change for
 
-// write code below that simulates the change machine in action!
+let pennies = READLINE.question('Please enter cents as a positive interger');
+
+//console.log(pennies);
+
+
+
+
+// declare, assign, and print how many quarters you can make out of the pennies
+
+let quaters = Math.floor(pennies/25);
+
+console.log(quaters, 'Quarters');
+ let remainder = pennies%25;
+
+
+// declare, assign, and print how many dimes you can make out of the pennies
+
+let dimes = Math.floor(remainder/10);
+
+console.log(dimes, 'Dimes');
+remainder = remainder%10;
+
+// declare, assign, and print how many nickels you can make out of the pennies
+
+let nickels = Math.floor(remainder/5);
+
+console.log(nickels, 'Nikels');
+
+
+
+
+// declare, assign, and print how many pennies are left over
+
+
+
+
+
+// make a change drawer using an object to
+// store all of your change
+// each property on the object should represent the particular coin
+// use the dot operator on the object to assign how many of each coin you have
+// many coins are in the drawer
+// print the drawer to see that the values are correct
---
+++ objects.js
@@ -0,0 +1,9 @@
+let miguel = {bornIn: 2003,
+              currentlyIn: "teamEdge",
+              goestoHighSchool: "true",
+              numberofSilblings: 2,
+              almostAGirl: "true",
+              catchPharse: "undefinded",
+              lovesLife: "false"
+};
+console.log(miguel);
